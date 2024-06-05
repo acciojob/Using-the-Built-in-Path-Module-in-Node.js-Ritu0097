a53285a4b3855483577387cac601c910911ea6bc
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-05 13:46:34 +0530

    joining

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const path = require('path');
 
-// Function to join two file path segments
 function joinPaths(segment1, segment2) {
   return path.join(segment1, segment2);
 }
 
-// Get the command-line arguments
 const [, , segment1, segment2] = process.argv;
 
-// Check if both segments are provided
 if (segment1 && segment2) {
   const joinedPath = joinPaths(segment1, segment2);
   console.log('Joined file path:', joinedPath);
